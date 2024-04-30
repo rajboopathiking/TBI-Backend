9ac473858e53ae8a23fb1528f7c68e4ac75336fd
Boopathi_DataNostradamus <51164330+rajboopathiking@users.noreply.github.com>
2024-04-30 12:53:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ async def predict(data: InputData):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run("main:app", port=8000, log_level="info")
