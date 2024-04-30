0a5a30f40bfa25f32d1a45a4139b87fe617bee43
Boopathi_DataNostradamus <51164330+rajboopathiking@users.noreply.github.com>
2024-04-30 12:09:50 +0530

    Delete Front-End directory

--- Front-End/DBI/src/components/About.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function About() {
-  return (
-    <div className='shadow-2xl p-2 bg-blue-300 ' id='About'>
-        <p className='font-bold text-black text-center'>## 2024 @copywrite</p>
-    </div>
-  )
-}
